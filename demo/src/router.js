af3815d0ffd307eccf0b7c794c6e8a0d8884b364
余珩 <1281005689@qq.com>
2018-04-24 16:22:30 +0800

    change route

--- demo/src/router.js
+++ demo/src/router.js
@@ -17,9 +17,9 @@ export default class Router extends React.Component {
   constructor(props) {
     super(props);
 
-    this.Post = lodable({
+    this.Echarts = lodable({
       loader: () => {
-        return import('./Post');
+        return import('./Echarts');
       },
       loading: () => {
         return <div>Loading...</div>;
@@ -34,7 +34,7 @@ export default class Router extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={this.Post} />
+          <Route exact path="/" component={this.Echarts} />
         </Switch>
       </BrowserRouter>
     );
